16f2bdc7e527c484b45174bc12764b73769f7be1
Züleyha Karali <zuleyha.karali04@gmail.com>
2025-10-17 06:45:26 +0300

    Son haliyle klasör yapısı köke taşındı ve kod güncellendi.

--- little_sort.c
+++ little_sort.c
@@ -1,8 +1,27 @@
 #include "push_swap.h"
 
-void find_min()
+int find_min_idx(t_list **a)
 {
+    int i;
+    int idx;
+    long min;
+    t_list *tmp;
 
+    i = 0;
+    idx = 0;
+    min = (*a)->content;
+    tmp = *a;
+    while (tmp != NULL)
+    {
+        if (min > tmp->content)
+        {
+            min = tmp->content;
+            idx = i;
+        }
+        tmp = tmp->next;
+        i++;
+    }
+    return (idx);
 }
 
 void for_three(t_list **a)
@@ -14,18 +33,18 @@ void for_three(t_list **a)
     fir = (*a)->content;
     sec = (*a)->next->content;
     thi = (*a)->next->next->content;
-    if (fir > sec && sec > thi)
+    if (fir > sec && sec > thi) // 3 2 1
     {
         sa(a);
         rra(a);
     }
-    else if (fir > thi && thi > sec)
+    else if (fir > thi && thi > sec) // 3 1 2
         ra(a);
-    else if (sec > fir && fir > thi)
+    else if (sec > fir && fir > thi) // 2 3 1
         rra(a);
-    else if (thi > fir && fir > sec) 
+    else if (thi > fir && fir > sec) // 2 1 3
         sa(a);
-    else if (sec > thi && thi > fir) 
+    else if (sec > thi && thi > fir) //1 3 2
     {
         sa(a);
         ra(a);
@@ -34,27 +53,22 @@ void for_three(t_list **a)
 
 void for_four(t_list **a, t_list **b)
 {
-    int min;
     int i;
     int idx;
-    t_list *tmp;
 
-    i = 0;
     idx = 0;
-    tmp = *a;
-    min = (*a)->content;
-    while (tmp)
+    idx = find_min_idx(a);
+    i = ft_lstsize(*a);
+    if (idx <= i - idx)
     {
-        if (min > tmp->content)
-        {
-            min = tmp->content;
-            idx = i;
-        }
-        tmp = tmp->next;
-        i++;
+        while (idx-- > 0)
+            ra(a);
     }
-    while (idx-- > 0)
-        ra(a);
+    else
+    {
+        while (idx++ < i)
+            rra(a);
+    }
     pb(b, a);
     for_three(a);
     pa(a, b);
@@ -62,27 +76,21 @@ void for_four(t_list **a, t_list **b)
 
 void for_five(t_list **a, t_list **b)
 {
-    int min;
     int i;
     int idx;
-    t_list *tmp;
 
-    i = 0;
-    idx = 0;
-    tmp = *a;
-    min = (*a)->content;
-    while (tmp)
+    i = ft_lstsize(*a);
+    idx = find_min_idx(a);
+    if (idx <= i - idx)
     {
-        if (min > tmp->content)
-        {
-            min = tmp->content;
-            idx = i;
-        }
-        tmp = tmp->next;
-        i++;
+        while (idx-- > 0)
+            ra(a);
     }
-    while (idx-- > 0)
-        ra(a);
+    else
+    {
+        while (idx++ < i)
+            rra(a);
+    }
     pb(b, a);
     for_four(a, b);
     pa(a, b);
--- push_swap/big_sort.c
+++
@@ -1,7 +0,0 @@
-#include "push_swap.h"
-
-void big_sort(t_list **a, t_list **b)
-{
-    (void)a;
-    (void)b;
-}
